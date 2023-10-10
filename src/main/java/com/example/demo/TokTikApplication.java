2219540e53297677d13bb65b755c610306994cb3
Alex Simon <aleandre6579.simon@gmail.com>
2023-10-10 21:52:42 +0700

    Made backend able to run with mariadb and redis

--- src/main/java/com/example/demo/TokTikApplication.java
+++ src/main/java/com/example/demo/TokTikApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.toktik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TokTikApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TokTikApplication.class, args);
 	}
 
 }
