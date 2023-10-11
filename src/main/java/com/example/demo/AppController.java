1c7dcb3845a9ff38472550d1b566db11a98f4559
Alex Simon <aleandre6579.simon@gmail.com>
2023-10-11 23:03:57 +0700

    Modified for Kubernetes implementation

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,15 +1,23 @@
 package com.javatpoint;  
-import org.springframework.web.bind.annotation.RequestMapping;  
+
+import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.RestController;  
 
 @RestController  
 public class AppController   
 {  
 
-    @RequestMapping("/")  
+    @GetMapping("/api/hello")
     public String hello()   
     {  
         return "Hello User";  
+    }
+
+
+    @GetMapping("/api")
+    public String api()   
+    {  
+        return "THIS IS HOME";  
     }
 
 }  
--- src/main/java/com/example/demo/TokTikApplication.java
+++ src/main/java/com/example/demo/TokTikApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TokTikApplication {
 
 	public static void main(String[] args) {
+        System.out.println("TEST LOGSS");
 		SpringApplication.run(TokTikApplication.class, args);
 	}
 
