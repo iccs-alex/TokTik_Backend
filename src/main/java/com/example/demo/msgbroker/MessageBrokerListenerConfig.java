e45e25a186550bb5887e2658beef9f5f81df97a2
Alex Simon <aleandre6579.simon@gmail.com>
2023-10-26 18:20:22 +0700

    Backend receives message when video workers are done

--- src/main/java/com/example/demo/msgbroker/MessageBrokerListenerConfig.java
+++ src/main/java/com/example/demo/msgbroker/MessageBrokerListenerConfig.java
@@ -1,30 +1,32 @@
-//package com.example.demo.msgbroker;
+package com.example.demo.msgbroker;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.MessageListener;
-//import org.springframework.data.redis.listener.ChannelTopic;
-//import org.springframework.data.redis.listener.PatternTopic;
-//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
-//@Configuration
-//public class MessageBrokerListenerConfig {
+@Configuration
+public class MessageBrokerListenerConfig {
 
-//@Bean
-    //public MessageListener myMessageListener() {
-    //    return new MessageReceiver();
-    //}
+@Bean
+    public MessageListener myMessageListener() {
+        return new MessageReceiver();
+    }
 
-   // @Bean
-   // public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
-   //     System.out.println("Creating channels");
-   //     RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-   //     container.setConnectionFactory(redisConnectionFactory);
-   //     container.addMessageListener(myMessageListener(), new ChannelTopic("convert"));
-   //     container.addMessageListener(myMessageListener(), new ChannelTopic("process"));
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+        System.out.println("Creating channels");
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(myMessageListener(), new ChannelTopic("convert"));
+        container.addMessageListener(myMessageListener(), new ChannelTopic("thumbnail"));
+        container.addMessageListener(myMessageListener(), new ChannelTopic("process"));
+        container.addMessageListener(myMessageListener(), new ChannelTopic("backend"));
         // Add more channels as needed
-   //     return container;
-   // }
-//}
+        return container;
+    }
+}
 
